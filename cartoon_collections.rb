3ac03f5f9549a8d2eebdcb8b55d6071122e0ae06
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-17 20:57:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -37,5 +37,4 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find { |mixed_calls| mixed_calls == valid_calls}
-  planeteer_calls.map |mixed_calls|
 end
